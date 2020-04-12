519fec6f2a8b325e1f60282642d96a55b10fb902
CXuanMing <48936815+CXuanMing@users.noreply.github.com>
2020-04-12 14:16:45 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -116,7 +116,7 @@
 		},
 		created() {
 			this.$http
-				.get("/public/data/home.json")
+				.get("public/data/home.json")
 				.then(({ data }) => {
 					this.ad = data.ad;
 					this.list = data.list;
